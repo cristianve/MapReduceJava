9d65516bc37aea0340b564ded4355dc7c23e9210
cvegasan <97cvega@gmail.com>
2019-02-26 19:41:13 +0100

    Pull MapReduce Base

---
+++ src/Inicializar.java
@@ -0,0 +1,15 @@
+import java.util.Hashtable; //List (k,v)
+import java.util.Vector; 
+
+// Permite añadir librerias Vectors, Hash exclusivamente
+// Requiere utilizacion de threads
+public class Inicializar {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Vector vec = new Vector();
+		Hashtable<String,String> hashtable = new Hashtable<String,String>();
+
+	}
+
+}
